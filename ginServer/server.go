f83d9bfc6fe80d69d0018290d645edcc4327f675
majh <majiahao@yiwei.com>
2023-04-13 16:18:35 +0800

    micro test

--- ginServer/server.go
+++ ginServer/server.go
@@ -1,11 +1,75 @@
 package main
 
 import (
+	"fmt"
 	"github.com/asim/go-micro/plugins/registry/consul/v3"
 	"github.com/asim/go-micro/v3/logger"
 	"github.com/asim/go-micro/v3/web"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"log"
+	"strconv"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+var userCons = make(map[string]map[*websocket.Conn]bool)
+
+func handleMessage(msg string, userId string) {
+	for conn, _ := range userCons[userId] {
+		err := conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("收到%s的消息%s", userId, msg)))
+		if err != nil {
+			log.Println("write msg err:", err)
+		}
+	}
+}
+
+func websocketHandler(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+	//userId := c.GetString("user_id")
+	userId, ok := c.GetQuery("user_id")
+	if !ok {
+		log.Println("user_id 为空 关闭连接")
+		_ = conn.Close()
+		return
+	}
+	if userCons[userId] == nil {
+		userCons[userId] = make(map[*websocket.Conn]bool)
+	}
+	userCons[userId][conn] = true
+	_ = conn.WriteMessage(websocket.TextMessage, []byte(userId+"连接成功"))
+	log.Println(userId + "连接数" + strconv.Itoa(len(userCons[userId])))
+	conn.SetCloseHandler(func(code int, text string) error {
+		delete(userCons[userId], conn)
+		log.Println(userId + "关闭连接")
+		log.Println(userId + "连接数" + strconv.Itoa(len(userCons[userId])))
+		return nil
+	})
+	for {
+		messageType, msg, err := conn.ReadMessage()
+		log.Println("msg type:", strconv.Itoa(messageType))
+		if err != nil {
+			_ = conn.Close()
+			log.Println("read msg err:", err)
+			return
+		}
+		log.Println("read msg:" + string(msg))
+		go handleMessage(string(msg), userId)
+
+		//err = conn.WriteMessage(websocket.TextMessage, []byte("请再说一遍"))
+		//if err != nil {
+		//	log.Println("write msg err:", err)
+		//}
+	}
+}
 
 func main() {
 	router := gin.Default()
@@ -16,6 +80,8 @@ func main() {
 			"message": "Hello, world!",
 		})
 	})
+
+	router.GET("/ws", websocketHandler)
 
 	service := web.NewService(
 		web.Name("gin-server"),
--- go.mod
+++ go.mod
@@ -36,6 +36,7 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gomodule/redigo v2.0.0+incompatible // indirect
 	github.com/google/uuid v1.2.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/consul/api v1.9.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
 	github.com/hashicorp/go-hclog v0.12.0 // indirect
