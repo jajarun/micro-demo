0f3fc7c1fa2f4f7e2f12c5dd066c5739fd0a12d6
majh <majiahao@yiwei.com>
2023-05-11 09:35:44 +0800

    micro test

--- ginServer/server.go
+++ ginServer/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/asim/go-micro/plugins/registry/consul/v3"
 	"github.com/asim/go-micro/v3/logger"
 	"github.com/asim/go-micro/v3/web"
 	"github.com/gin-gonic/gin"
@@ -81,9 +80,9 @@ func main() {
 
 	service := web.NewService(
 		web.Name("gin-server"),
-		web.Address("127.0.0.1:8888"),
+		web.Address("127.0.0.1:80"),
 		web.Handler(router),
-		web.Registry(consul.NewRegistry()),
+		//web.Registry(consul.NewRegistry()),
 	)
 
 	_ = service.Run()
